862e661442b3c3d29c32fbe4e1072552cce75acd
JiangQiqing <chashao0610@gmail.com>
2020-03-13 09:18:31 +0800

    优化

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,25 +1,25 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Horizen from '../../baseUI/horizen-item'
 import { categoryTypes, alphaTypes } from '../../api/config'
-import { 
+import {
   NavContainer,
   ListContainer,
   List,
   ListItem,
 } from "./style"
-import { 
-  getSingerList, 
-  getHotSingerList, 
-  changeEnterLoading, 
-  changePageCount, 
-  refreshMoreSingerList, 
-  changePullUpLoading, 
-  changePullDownLoading, 
-  refreshMoreHotSingerList 
+import {
+  getSingerList,
+  getHotSingerList,
+  changeEnterLoading,
+  changePageCount,
+  refreshMoreSingerList,
+  changePullUpLoading,
+  changePullDownLoading,
+  refreshMoreHotSingerList
 } from './store/actionCreators'
-import  LazyLoad, {forceCheck} from 'react-lazyload'
+import LazyLoad, { forceCheck } from 'react-lazyload'
 import Scroll from './../../baseUI/scroll/index'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import Loading from '../../baseUI/loading'
 
 function Singers(props) {
@@ -31,7 +31,9 @@ function Singers(props) {
   const { getHotSingerDispatch, updateDispatch, pullDownRefreshDispatch, pullUpRefreshDispatch } = props
 
   useEffect(() => {
-    getHotSingerDispatch()
+    if (!singerList.size) {
+      getHotSingerDispatch()
+    }
     // eslint-disable-next-line
   }, [])
 
@@ -54,17 +56,17 @@ function Singers(props) {
   }
 
   const renderSingerList = () => {
-    const list = singerList ? singerList.toJS(): []
+    const list = singerList ? singerList.toJS() : []
     console.log(props)
     return (
       <List>
         {
           list.map((item, index) => {
             return (
-              <ListItem key={item.accountId+""+index}>
+              <ListItem key={item.accountId + "" + index}>
                 <div className="img_wrapper">
-                  <LazyLoad placeholder={<img width="100%" height="100%" src={require('./singer.png')} alt="music"/>}>
-                    <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music"/>
+                  <LazyLoad placeholder={<img width="100%" height="100%" src={require('./singer.png')} alt="music" />}>
+                    <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music" />
                   </LazyLoad>
                 </div>
                 <span className="name">{item.name}</span>
@@ -81,18 +83,18 @@ function Singers(props) {
       <NavContainer>
         <Horizen list={categoryTypes} title={"分类(默认热门):"} handleClick={(val) => handleUpdateCatetory(val)} oldVal={category}></Horizen>
         <Horizen list={alphaTypes} title={"首字母:"} handleClick={val => handleUpdateAlpha(val)} oldVal={alpha}></Horizen>
-      </NavContainer> 
+      </NavContainer>
       <ListContainer>
         <Scroll
-          pullUp={ handlePullUp }
-          pullDown = { handlePullDown }
-          pullUpLoading = { pullUpLoading }
-          pullDownLoading = { pullDownLoading }
+          pullUp={handlePullUp}
+          pullDown={handlePullDown}
+          pullUpLoading={pullUpLoading}
+          pullDownLoading={pullDownLoading}
           onScroll={forceCheck}
         >
-          { renderSingerList() }
+          {renderSingerList()}
         </Scroll>
-        { enterLoading ? <Loading /> : null }
+        <Loading show={enterLoading}></Loading>
       </ListContainer>
     </div>
   )
@@ -119,8 +121,8 @@ const mapDispatchToProps = (dispatch) => {
     // 滑到最底部刷新部分的处理
     pullUpRefreshDispatch(category, alpha, hot, count) {
       dispatch(changePullUpLoading(true))
-      dispatch(changePageCount(count+1))
-      if(hot){
+      dispatch(changePageCount(count + 1))
+      if (hot) {
         dispatch(refreshMoreHotSingerList())
       } else {
         dispatch(refreshMoreSingerList(category, alpha))
@@ -130,13 +132,13 @@ const mapDispatchToProps = (dispatch) => {
     pullDownRefreshDispatch(category, alpha) {
       dispatch(changePullDownLoading(true))
       dispatch(changePageCount(0))
-      if(category === '' && alpha === ''){
+      if (category === '' && alpha === '') {
         dispatch(getHotSingerList())
       } else {
         dispatch(getSingerList(category, alpha))
       }
     }
   }
-}   
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Singers)
